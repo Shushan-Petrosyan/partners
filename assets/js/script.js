5975707a754a32b09b891161588835dc9e51b57f
Shushan Petrosyan <shushan.petrosyan@outlook.com>
2024-10-10 15:12:54 +0400

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,36 @@
+let a = 1;
+document.getElementById("seeMoreBtn").onclick = function() {
+    
+    
+    const morePictures = document.getElementsByClassName("hidden");
+    
+    
+    if (a % 2 == 0) {a++;
+        for (let i = 0; i < morePictures.length; i++) {
+            morePictures[i].style.display = "none";
+        }
+        this.textContent = "Показать еще";
+    } else {
+        a++;
+        for (let i = 0; i < morePictures.length; i++) {
+            morePictures[i].style.display = "block";
+        }
+        this.textContent = "Скрыть";
+    }
+};
+
+const swiper = new Swiper('.swiper', {
+  grabCursor : true,
+ pagination: {
+      el: '.swiper-pagination',
+      clickable : true,
+    },
+ 
+  
+  
+  });
+  
+
+
+
+
